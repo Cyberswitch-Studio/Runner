898287c353132e167c10357ebf49b584a4e6f11b
Steked <szymonjedryczko1@gmail.com>
2021-06-01 22:54:25 +0200

    Dodano ruch na boki między trzeba ścieżkami

--- Runner/Assets/Scripts/Movement.cs
+++ Runner/Assets/Scripts/Movement.cs
@@ -7,6 +7,9 @@ public class Movement : MonoBehaviour
     private CharacterController controller;
     private Vector3 direction;
     public float forwardSpeed;
+
+    private int currentLane = 1;
+    public float lineDistance = 4;
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -15,7 +18,38 @@ public class Movement : MonoBehaviour
     void Update()
     {
         direction.z = forwardSpeed;
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            currentLane++;
+            if (currentLane == 3)
+                currentLane = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            currentLane--;
+            if (currentLane == -1)
+                currentLane = 0;
+        }
+
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+
+        if (currentLane == 0)
+        {
+            targetPosition += Vector3.left * lineDistance;
+
+        }
+        
+        else if (currentLane == 2)
+
+        {
+            targetPosition += Vector3.right * lineDistance;
+        }
+
+        transform.position = targetPosition;
     }
+
 
     private void FixedUpdate()
     {
