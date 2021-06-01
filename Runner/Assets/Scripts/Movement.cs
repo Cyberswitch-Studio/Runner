3ab524d0f8bfbbae20eac8404ef627b6a35336ef
Steked <szymonjedryczko1@gmail.com>
2021-06-01 20:22:32 +0200

    Dodano ruch gracza do przodu

---
+++ Runner/Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 direction;
+    public float forwardSpeed;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        direction.z = forwardSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(direction * Time.fixedDeltaTime);
+    }
+}
