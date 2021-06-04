a3c1f560c50e2d452e28a37168fdc3519affe4aa
BartyDash <writetobartek@gmail.com>
2021-06-04 15:28:03 +0200

    Gładki ruch gracza na boki + poprawienie wartości

--- Runner/Assets/Scripts/Player_movement.cs
+++ Runner/Assets/Scripts/Player_movement.cs
@@ -7,8 +7,8 @@ public class Player_movement : MonoBehaviour
     private CharacterController controller;
     private Vector3 direction;
     public float forwardSpeed;
-    private int currentLane = 1; // 0 - left path, 1 - middle path, 2 - right path
-    public float lineDistance = 4;
+    private int currentPath = 1; // 0 - left path, 1 - middle path, 2 - right path
+    public float lineDistance = 3.33f;
 
     void Start()
     {
@@ -21,33 +21,45 @@ public class Player_movement : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            currentLane++;
-            if (currentLane == 3)
-                currentLane = 2;
+            currentPath++;
+            if (currentPath == 3)
+                currentPath = 2;
         }
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            currentLane--;
-            if (currentLane == -1)
-                currentLane = 0;
+            currentPath--;
+            if (currentPath == -1)
+                currentPath = 0;
         }
 
         Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
 
-        if (currentLane == 0)
+        if (currentPath == 0)
         {
             targetPosition += Vector3.left * lineDistance;
 
         }
         
-        else if (currentLane == 2)
+        else if (currentPath == 2)
 
         {
             targetPosition += Vector3.right * lineDistance;
         }
 
-        transform.position = targetPosition;
+        //transform.position = targetPosition;
+        if (transform.position == targetPosition)
+            return;
+        Vector3 diff = targetPosition - transform.position;
+        Vector3 moveDir = diff.normalized * 25 * Time.deltaTime;
+        if (moveDir.sqrMagnitude < diff.sqrMagnitude)
+        {
+            controller.Move(moveDir);
+        }
+        else
+        {
+            controller.Move(diff);
+        }
     }
 
     private void FixedUpdate()
